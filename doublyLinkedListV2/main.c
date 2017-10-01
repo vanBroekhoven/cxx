78f79f7d2cde6dc50b9079ee76afd30785a4a63d
vanBroekhoven <timvanbroekhoven@outlook.com>
2017-10-01 17:58:23 +0200

    added Unit-test 1: test creation of new list

--- doublyLinkedListV2/main.c
+++ doublyLinkedListV2/main.c
@@ -10,7 +10,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "doublyLinkedListV2_lib.h"
-#include "munit.h"  -> 'micro-unit framework for testing code' https://nemequ.github.io/munit/
+#include "munit.h"  //'micro-unit framework for testing code'
+
+/*UNIT-TEST 1: function for creation of a list*/
+//Testing if head & tail pointer == NULL
+static MunitResult
+test_createNewList(const MunitParameter params[], void* user_data) {
+	(void) params;
+	(void) user_data;
+
+	//??
+
+	//generate new list
+
+	//??
+
+	//free memory
+
+    return MUNIT_OK;
+}
 
 int main()
 {
